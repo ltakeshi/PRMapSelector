8298b799da7b67d9103a57d651510bcb3641cb13
ltakeshi <tuxtakeshi@gmail.com>
2013-09-03 00:26:37 +0900

    add SelectorController::initialize

--- app/controllers/selector_controller.rb
+++ app/controllers/selector_controller.rb
@@ -1,15 +1,13 @@
 class SelectorController < ApplicationController
-#    def initialize
-#        @map_all = Map::Map_List
-#        @map_rule = Map::Map_Rule
-#    end
+    def initialize
+        super
+        @map_all = Map::Map_List
+        @map_rule = Map::Map_Rule
+    end
   def index
-      @map_all = Map::Map_List
       @root = Rails.root
   end
   def select
-      @map_all = Map::Map_List
-      @map_rule = Map::Map_Rule
       @map_names = params[:map].select{|k,v| v == "1"}.keys
   end
   def result
